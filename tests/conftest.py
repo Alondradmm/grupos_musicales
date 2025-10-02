a703a582c66c35562e8a68ed9699a86cbe5221b7
Alondradmm <dalon0904@gmail.com>
2025-10-01 18:42:24 -0600

    Test falla

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,22 +3,22 @@ from app import create_app
 from unittest.mock import MagicMock
 
 @pytest.fixture
-def app():
-    """
-    Crea la app Flask en modo testing y la devuelve para usarla en los tests.
-    La conexión a MySQL se reemplaza por un mock.
-    """
-    app = create_app(testing=True)
+# def app():
+#     """
+#     Crea la app Flask en modo testing y la devuelve para usarla en los tests.
+#     La conexión a MySQL se reemplaza por un mock.
+#     """
+#     app = create_app(testing=True)
 
-    # Mock global de MySQL
-    app.mysql = MagicMock()
-    cursor_mock = MagicMock()
-    app.mysql.cursor.return_value = cursor_mock
-    # fetchone y fetchall devolverán datos por test específico con side_effect
-    cursor_mock.fetchone.return_value = None
-    cursor_mock.fetchall.return_value = []
+#     # Mock global de MySQL
+#     app.mysql = MagicMock()
+#     cursor_mock = MagicMock()
+#     app.mysql.cursor.return_value = cursor_mock
+#     # fetchone y fetchall devolverán datos por test específico con side_effect
+#     cursor_mock.fetchone.return_value = None
+#     cursor_mock.fetchall.return_value = []
 
-    yield app
+#     yield app
 
 @pytest.fixture
 def client(app):
